9916a318b859fe5519755d7d7e4337db64585939
Brendan Malycky <bmalycky@hotmail.com>
2024-05-02 12:29:07 -0600

    added show/hide functionality

--- public/js/client.js
+++ public/js/client.js
@@ -13,15 +13,27 @@ const preventEnterKeySubmit = () => {
         }
 }
 
-preventEnterKeySubmit();
-
-const testing = () => {
+const showCreateForm = function() {
   for (let i = 1; i < 4; i++) {
     let targetElement = document.getElementById(`action${i}`);
     targetElement.addEventListener("click", (event) => {
       console.log('clicked', event.target.id);
+      if (event.target.id === "action1") {
+        let element = document.getElementById("action1-container");
+        element.classList.remove("visible-no");
+        hideOnSubmit();  
+      }
     })
   }  
 }
 
-testing();
+const hideOnSubmit = () => {
+  let element = document.getElementsByClassName("post-button")[0];
+  element.addEventListener("click", () => {
+    let element2 = document.getElementById("action1-container");
+    element2.classList.add("visible-no");
+  })
+}
+
+showCreateForm();
+preventEnterKeySubmit();
