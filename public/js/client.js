4c9e084c247e4f93fa14a9f178cc139c86e09ba4
Brendan Malycky <bmalycky@hotmail.com>
2024-05-03 03:05:37 -0600

    made only selected form show up when option selected

--- public/js/client.js
+++ public/js/client.js
@@ -1,3 +1,5 @@
+let actionShowing = {action1: false, action2: false, action3: false};
+
 // prevents enter button from submitting a form //
 // get the form element//
 
@@ -13,21 +15,44 @@ const preventEnterKeySubmit = () => {
         }
 }
 
+const hideAction = function(obj) {
+  if (obj.action1 === true && (obj.action2 || obj.action3 === true)) {
+    let element = document.getElementById("action1-container");
+    element.classList.add("visible-no");
+    obj.action1 = false;
+  } else if (obj.action2 === true && (obj.action1 || obj.action3 === true)) {
+    let element = document.getElementById("action2-container");
+    element.classList.add("visible-no");
+    obj.action2 = false;
+  } 
+  console.log();
+}
+
+
+
+// when a new option is selected it should hide the currently displayed action/form //
+
 const showActionForm = function() {
+  let element1 = document.getElementById("action1-container");
+  let element2 = document.getElementById("action2-container");
+  //let element3 = document.getElementById("action3-container");
   for (let i = 1; i < 4; i++) {
     let targetElement = document.getElementById(`action${i}`);
     targetElement.addEventListener("click", (event) => {
       console.log('clicked', event.target.id);
+      console.log('obj', actionShowing);
       if (event.target.id === "action1") {
-        let element = document.getElementById("action1-container");
-        element.classList.remove("visible-no");  
+        element1.classList.remove("visible-no");
+        element2.classList.add("visible-no"); 
       } else if (event.target.id === "action2") {
-        let element = document.getElementById("action2-container");
-        element.classList.remove("visible-no");
+        element1.classList.add("visible-no");
+        element2.classList.remove("visible-no");
       }
     })
   }  
 }
+
+
 
 // const hideOnSubmit = () => {
 //   let element = document.getElementsByClassName("post-button")[0];
