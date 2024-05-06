6527c07174fcfa09d62ea3aeef1972444764632a
Brendan Malycky <bmalycky@hotmail.com>
2024-05-06 13:49:48 -0600

    added comments to server file

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const addPost = function(req) {
   // only pushes into array if object is not empty //
   if (Object.keys(req.body).length > 0 ) {
     let idNum = Math.ceil(Math.random() * 50000);
+    // adds new key value pair to object //
     Object.assign(req.body, {id: idNum});
     Object.assign(req.body, {deleteId: idNum});
     postsArr.push(req.body);
@@ -16,24 +17,27 @@ const addPost = function(req) {
 }
 
 const editPost = function(req) {
+  // getting the id from data sent in post request
   const postId = req.body.id;
-  //const post = postsArr.filter((foundPost) => foundPost.id == postId);
-  //console.log("original post found with filter", post);
-  //console.log("the entire postsArr", postsArr);
-  console.log("index of", postsArr.findIndex((element) => element.id == postId));
+  // getting the index of the post in the array that matches the id in the post request 
   let index = postsArr.findIndex((element) => element.id == postId);
+  // replacing the element at that index with the object sent in post request 
   postsArr[index] = req.body;
 }
 
+// needed to serve static files 
 app.use(express.static("public"));
+// gives access to req.body object 
 app.use(express.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
+  // sending file and the array to be used to render data in the html(ejs file)
   res.render("index.ejs", { posts: postsArr });
 })
 
 app.get("/server-data", (req, res) => {
+  // sending array data for my fetch method so i can use the array data in the client side js file  
   res.send({data: postsArr});
 })
 
@@ -55,7 +59,6 @@ app.post("/create", (req, res) => {
 })
 
 app.post("/edit", (req, res) => {
-  console.log("new data to update with", req.body);
   editPost(req);
   res.render("blog-posts.ejs", { posts: postsArr });
 })
